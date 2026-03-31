ff9b8310fb8031bbd3997a7def02e7549fc83471
Faruque Abdullah <abdullahfaruque007@gmail.com>
2026-03-31 11:57:39 +0530

    change global file to addContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
-import Global from "./Global/Global";
+
 import Loginpage from "./pages/login/Loginpage";
 import Signuppage from "./pages/signup/Signuppage";
 import Home from "./pages/home/Home";
 import Members from "./pages/members/Members";
 import Books from "./pages/books/Books";
 import Checkouts from "./pages/checkouts/Checkouts";
+import Global from "./appContainer/Global";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Global />,
+      element: < Global/>,
       children: [
         {
           path: "/",
